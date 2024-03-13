3238c3c188be5ac2f6cd87f5abb65da445186e05
Cristiano Mbongo <cristianombongo@refera.com.br>
2024-03-13 17:52:40 +0100

    send project

---
+++ src/test/java/ao/magnodev/develop/DevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.magnodev.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
